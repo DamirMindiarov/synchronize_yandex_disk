6b3338de45bbb38ae510287ed50c962d678b118a
DamirMindiarov <akacuki1263547@gmail.com>
2025-07-21 21:39:09 +0700

    add main.py func.py

---
+++ func.py
@@ -0,0 +1,46 @@
+import os
+
+from config import PATH_TO_SYNC_FOLDER
+from synctodisk import SyncToDisk
+
+
+def get_all_file_paths(directory: str) -> list[dict]:
+    """Получить все файлы в указанной директории"""
+    file_paths = []
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        file_paths.append({"file_path": filepath,
+                           "last_modified": os.path.getmtime(filepath),
+                           "filename": filename})
+    return file_paths
+
+
+def detect_file_change(file_path: str, last_modified: float) -> tuple[bool, float]:
+    """Проверяет изменен файл"""
+    current_modified = os.path.getmtime(file_path)
+    return last_modified != current_modified, current_modified
+
+
+def synchronize_with_disk(all_files: list[dict], my_disk: SyncToDisk):
+    """Синхронизируется с диском(удаляет лишнее, добавляет отсутствующее)"""
+    all_filenames = [file["filename"] for file in all_files]
+
+    try:
+        all_filenames_on_disk = [file["name"] for file in
+                                 my_disk.get_info()]
+    except TypeError:
+        all_filenames_on_disk = None
+
+    if all_filenames_on_disk is not None:
+        files_needs_upload = set(all_filenames).difference(
+            set(all_filenames_on_disk))
+        files_needs_to_delete = set(all_filenames_on_disk).difference(
+            set(all_filenames))
+
+        for file in files_needs_to_delete:
+            my_disk.delete(file)
+
+        for file in files_needs_upload:
+            my_disk.load(PATH_TO_SYNC_FOLDER + file)
+
+
---
+++ main.py
@@ -0,0 +1,52 @@
+import time
+
+from loguru import logger
+
+from config import TOKEN_DISK, PATH_TO_SYNC_FOLDER, PATH_TO_FOLDER_IN_DISK, \
+    PERIOD_SYNC, PATH_TO_LOG_FILE
+from func import get_all_file_paths, detect_file_change, synchronize_with_disk
+from synctodisk import SyncToDisk
+
+logger.add(f"{PATH_TO_LOG_FILE}/file.log", format="{time} {level} {message}")
+
+
+def main():
+    my_disk = SyncToDisk(token=TOKEN_DISK, dirpath=PATH_TO_FOLDER_IN_DISK)
+
+    all_files = get_all_file_paths(PATH_TO_SYNC_FOLDER)
+    synchronize_with_disk(all_files, my_disk)
+
+    while True:
+
+        for file in all_files:
+            try:
+                changed, last_modified = detect_file_change(
+                    file_path=file["file_path"],
+                    last_modified=file["last_modified"],
+                )
+                file["last_modified"] = last_modified
+                if changed:
+                    my_disk.reload(path=file["file_path"])
+            except FileNotFoundError:
+                continue
+
+        all_files = get_all_file_paths(PATH_TO_SYNC_FOLDER)
+        synchronize_with_disk(all_files, my_disk)
+
+        time.sleep(PERIOD_SYNC)
+
+
+
+
+if __name__ == '__main__':
+    logger.info(f"Программа синхронизации файлов начинает работу с директорией {PATH_TO_SYNC_FOLDER}")
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.info("Программа синхронизации файлов завершила работу")
+
+
+
+
+
